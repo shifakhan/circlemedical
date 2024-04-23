9a2c2cd68cb3461c2921ed4bcae6f4bcbf7a79c2
shifakhan <findshifa@gmail.com>
2024-04-23 15:57:50 -0400

    Add a model method to search movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,4 +11,17 @@ class Movie
   validates :summary, presence: true
   validates :year, presence: true
   validates :rating, presence: true
+
+  def self.search(options)
+    valid_options = options.slice(:query)
+    return { status: :unprocessable_entity, error: "Invalid params" } if valid_options.empty?
+
+    result = MovieDbApi.new.search(valid_options)
+    if result[:error].present?
+      # Call another movie API used as fallback. For now return an error message
+      return { status: :service_unavailable, error: "Service Unavailable" }
+    end
+
+    { status: :ok, data: result[:data] }
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -24,4 +24,36 @@ RSpec.describe Movie, type: :model do
       expect(movie.errors.messages).to eq(errors)
     end
   end
+
+  describe 'search' do
+    it 'returns an error if missing options' do
+      expect(Movie.search(random: {})).to eq({ status: :unprocessable_entity, error: "Invalid params" })
+    end
+
+    it 'returns service unavailable if all APIs are down' do
+      movie_db_api = double("MovieDbApi")
+      allow(movie_db_api).to receive(:search).and_return(
+        { status: 401, error: 'Unauthorized'}
+      )
+      allow(MovieDbApi).to receive(:new).and_return(movie_db_api)
+
+      expect(Movie.search(query: "batman")).to eq({ status: :service_unavailable, error: "Service Unavailable" })
+    end
+
+    it 'returns movies if search is successful' do
+      movie = Movie.new(
+        title: "Jack Reacher: Never Go Back",
+        summary: "Jack Reacher must uncover the truth behind a major government conspiracy in order to clear his name. On the run as a fugitive from the law, Reacher uncovers a potential secret from his past that could change his life forever.",
+        year: 2016,
+        rating: 4.19
+      )
+      movie_db_api = double("MovieDbApi")
+      allow(movie_db_api).to receive(:search).and_return(
+        { status: 200, data: [movie] }
+      )
+      allow(MovieDbApi).to receive(:new).and_return(movie_db_api)
+
+      expect(Movie.search(query: "jack")).to eq({ status: :ok, data: [movie] })
+    end
+  end
 end
